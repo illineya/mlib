cf51d1a5833f3ca33a4aef5d98662fa4ca16db0c
Igor Medvedev <ulcigor@MacBook-Air-Igor.local>
2023-02-08 23:38:45 +0300

    Добавлены коментарии к срутинам

--- include/mroutine.h
+++ include/mroutine.h
@@ -30,8 +30,28 @@ struct MRoutine {
     MQueue_t *queue;
 };
 
+/**
+ * Создаёт новый пул рутин
+ *
+ * @return MRoutine_t *
+ */
 MRoutine_t *mroutine_init();
+
+/**
+ * Додавляет задачу в рутину
+ *
+ * @param routine MRoutine_t *
+ * @param func MThreadFunc
+ * @param data mpointer_t
+ * @param destroy MThreadDestroyFunc
+ */
 void mroutine_attach(MRoutine_t *routine, MThreadFunc func, mpointer_t data, MThreadDestroyFunc destroy);
+
+/**
+ * Ощичает выделенную под рутины память
+ *
+ * @param routine MRoutine_t *
+ */
 void mroutine_deinit(MRoutine_t *routine);
 
 #endif
